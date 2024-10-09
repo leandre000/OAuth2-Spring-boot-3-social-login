b4d678a67595d21e9ac1b82d170f06fbe603e8e8
Leandre <iamshemaleandre@gmail.com>
2024-10-09 08:28:50 +0200

    social login implementation

---
+++ src/main/java/com/leandre/social/security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.leandre.social.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login();
+        return http.build();
+    }
+}
